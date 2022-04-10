2f268c800cbf89ccdc03236f2f0e23a23fb5b465
Marios Tsaltakis <mariostsaltakis@gmail.com>
2022-04-10 18:40:58 +0300

    Update Admin.java

--- Project Altruism AM-1084516-1084517-1084549/Project/Admin.java
+++ Project Altruism AM-1084516-1084517-1084549/Project/Admin.java
@@ -2,7 +2,7 @@ public class Admin extends User {
 
     private static boolean isAdmin = true ;
 
-    public boolean getAdmin() // return the information that the user is admin
+    public boolean getAdmin() // return the information that the user is admin.
     {
         return isAdmin;
     }
